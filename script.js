2941d83d55adc0e03de292f6550290013be3d4d8
Mohamed Waleed <114451140+Mowaleed2@users.noreply.github.com>
2023-02-28 22:53:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var currentCategory= ['history', 'language', 'nature', 'technology'];
 var Questions= [
 	// store answer with questions for easier retrieval
-	{ category: 'history', question : 'In star star program for agents evaluation for all over sales is 40% ', answer: false },
-	{ category: 'history', question: 'Agent customer experience in all star star program , TNPS gets 20% from the evaluation', answer: false },
-	{ category: 'history', question: 'In final commission Accelerator if you get 102% final commission you will increase 10 % extra ', answer: false },
-	{ category: 'history', question: 'Productivity & Operation wight on KPI is 28%', answer: true },
-	{ category: 'history', question: 'in mystery evaluation if you get 82% you will get 100% over all score ', answer: true },
+	{ category: '', question : 'In star program for agents evaluation for all over sales is 40% ', answer: false },
+	{ category: '', question: 'Agent customer experience in all star star program , TNPS gets 20% from the evaluation', answer: false },
+	{ category: '', question: 'In final commission Accelerator if you get 102% final commission you will increase 10 % extra ', answer: false },
+	{ category: '', question: 'Productivity & Operation wight on KPI is 28%', answer: true },
+	{ category: '', question: 'in mystery evaluation if you get 82% you will get 100% over all score ', answer: true },
 
 ];
 
